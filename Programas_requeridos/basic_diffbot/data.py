049e0374f27d28d9a7556e290706ee786ac82c68
ErickNoeRamirezOlvera <74927243+ErickNoeRamirezOlvera@users.noreply.github.com>
2023-11-22 13:03:51 -0600

    Add files via upload

---
+++ Programas_requeridos/basic_diffbot/data.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import rclpy
+import mysql.connector
+import numpy as np
+from rclpy.node import Node
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32
+from std_msgs.msg import Int32
+from rclpy.qos import ReliabilityPolicy, QoSProfile
+from sensor_msgs.msg import JointState
+
+from datetime import datetime
+
+class sql(Node):
+	def __init__(self):
+		super().__init__('sql')
+		self.subscriber = self.create_subscription(Odometry, '/odom', self.odom_callback,QoSProfile(depth=10,reliability=ReliabilityPolicy.RELIABLE))
+		
+		self.subscriber = self.create_subscription(Twist, '/cmd_vel', self.vel_callback,QoSProfile(depth=10,reliability=ReliabilityPolicy.RELIABLE))
+		
+		timer_period = 3.0
+		self.tmr = self.create_timer(timer_period, self.timer_callback)
+        
+	def odom_callback(self,msg):
+		self.odom = msg
+		self.x = self.odom.pose.pose.position.x
+		self.y = self.odom.pose.pose.position.y
+		#self.get_logger().info("x: "+ str(self.x) + "y: " + str(self.y))
+		
+	def vel_callback(self,msg):
+		self.twist = msg
+		self.x_vel = self.twist.linear.x
+		self.y_vel = self.twist.linear.y
+		self.z_vel = self.twist.angular.z		
+		
+	def timer_callback(self):
+		try:
+			connection = mysql.connector.connect(
+				host = 'helgedb.cjgw7hhcigld.us-east-1.rds.amazonaws.com',
+				port = 3306,
+				user = 'admin',
+				password = '49katkashi',
+				database='HelgenTable'
+			)
+			cursor = connection.cursor()	
+			time = datetime.now()
+			
+			# Comandos SQL para insertar valores
+			sql_insert = '''
+					INSERT INTO HelgenData(x_, y_, vx, vy, vz)
+					VALUES(%s, %s, %s, %s, %s);
+            			     '''
+			
+			data = self.x, self.y, self.x_vel, self.y_vel, self.z_vel
+			#data = 0.1, 0.2
+			
+			try:
+			    self.get_logger().info("x_pose: "+ str(self.x) + " y_pose: " + str(self.y) + " x_vel: " + str(self.x_vel) + " y_vel: " + str(self.y_vel) + " z_vel: " + str(self.z_vel))
+			    cursor.execute(sql_insert, data)
+			    connection.commit()
+			    
+			except mysql.connector.Error as err:
+			    self.get_logger().info("Error al enviar datos...")
+			    #conn.rollback()
+
+			# Cierra la conexión
+			cursor.close()
+		except:
+			self.get_logger().info("Esperando movimiento del montacargas... Si el montacargas ya esta en movimiento, revisar conectividad")
+
+def main(args=None):
+	rclpy.init(args=args)
+	node = sql()
+
+	rclpy.spin(node)
+	node.destroy_node()
+	rclpy.shutdown()
+if __name__ == '__main__':
+  main()
---
+++ Programas_requeridos/basic_diffbot/red.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import torch
+from roboflow import Roboflow
+import numpy as np
+
+class red(Node):
+  def __init__(self):
+    super().__init__('red')
+    #Suscripcion topico imagen
+    self.subscription = self.create_subscription(
+      Image, 
+      '/camera/image_raw', 
+      self.listener_callback, 
+      10)
+    self.subscription
+    self.br = CvBridge()
+    
+    #Obtencion de red neuronal
+    self.model = torch.hub.load('ultralytics/yolov5', 'custom', path = '/home/erick/proyect_ws/best.pt')
+    self.classes = self.model.names
+    self.model.conf = 0.60
+    
+    #Publicar resultados
+    self.publisher = self.create_publisher(Image, 'red', 10)
+    
+    timer_period = 0.1  # seconds
+    self.timer = self.create_timer(timer_period, self.red_callback)
+  
+  def class_to_label(self, x):
+    return self.classes[int(x)]
+   
+  def listener_callback(self, data):
+    self.frame = self.br.imgmsg_to_cv2(data)
+    
+    dim = (416, 416)
+    self.frame = cv2.resize(self.frame, dim, interpolation = cv2.INTER_AREA)
+
+  def red_callback(self):
+    try:
+    	self.detect = self.model(self.frame)
+    	labels, cord = self.detect.xyxyn[0][:, -1], self.detect.xyxyn[0][:, :-1]
+    	n = len(labels)
+    	x_shape, y_shape = self.frame.shape[1], self.frame.shape[0]
+    	for i in range(n):
+    		row = cord[i]
+    		if row[4] >= 0.3:
+    			x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+    			bgr = (0, 255, 0)
+    			cv2.rectangle(self.frame, (x1, y1), (x2, y2), bgr, 2)
+    			cv2.putText(self.frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+    	try:
+    		self.publisher.publish(self.br.cv2_to_imgmsg(self.frame, "bgr8"))
+    	except CvBridgeError as e:
+    		self.get_logger().info(e)
+    except:
+    	self.get_logger().info("Cargando detección...")
+
+def main(args=None):
+
+  rclpy.init(args=args)
+
+  image_subscriber = red()
+
+  rclpy.spin(image_subscriber)
+  
+  image_subscriber.destroy_node()
+
+  rclpy.shutdown()
+  
+if __name__ == '__main__':
+  main()
